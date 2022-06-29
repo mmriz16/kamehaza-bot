2e05399c781e8f10e78902638f718ee6ed8024b2
mmriz16 <m.miftakhul.rizky4@gmail.com>
2022-06-29 16:29:51 +0700

    add welcome image

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const Discord = require("discord.js")
 require("dotenv").config()
 
-const TOKEN = "OTkxNjA0NDA1MjczMTE2Njcy.G_ZRKW.wbcE9aOrN0dSNo5GG7c-G624ECoRIgTuEomURU"
+const generateImage = require("./generate_Image")
 
 const client = new Discord.Client({
     intents: [
         "GUILDS",
-        "GUILD_MESSAGES"
+        "GUILD_MESSAGES",
+        "GUILD_MEMBERS"
     ]
 })
 
@@ -15,9 +16,26 @@ client.on("ready", () => {
 })
 
 client.on("messageCreate", (message) => {
-    if (message.content == "hi"){
-        message.reply("Hello World!")
+    if (message.content == "hi") {
+        message.reply("Hallo!")
+    } else if (message.content == "hallo ganteng") {
+        message.reply("Hallo juga ganteng😊")
+    } else if (message.content == "lagi apa?") {
+        message.reply("Lagi mikirin kamu 😘")
+    } else if (message.content == "asu") {
+        message.reply("Jangan ngomong kasar ya 😉")
     }
+})
+
+const welcomeChannelId = "991610772377583657"
+const generalChannelId = "990829093434056711"
+
+client.on("guildMemberAdd", async (member) => {
+    const img = await generateImage(member)
+    member.guild.channels.cache.get(welcomeChannelId).send({
+        content: `<@${member.id}> Welcome to the server!`,
+        files: [img]
+    })
 })
 
 client.login(process.env.TOKEN)
